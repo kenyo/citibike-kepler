229ff1544b7b2e09f98d3ef61fd70808e3247929
kenny oh <kennyoh517@gmail.com>
2020-02-16 22:09:12 -0500

    add catchall for react app in express router

--- server/app.js
+++ server/app.js
@@ -22,7 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
