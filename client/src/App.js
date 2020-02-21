c5bb5e74aad13f50798e4a0434ae8505b927e6cd
kenny oh <kennyoh517@gmail.com>
2020-02-21 16:56:57 -0500

    add tests for static data serving

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch('/data/dailyBase.json').then(console.log)
   return (
     <div className="App">
       <header className="App-header">
--- server/app.js
+++ server/app.js
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(express.static(path.join(__dirname, 'data')));
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
