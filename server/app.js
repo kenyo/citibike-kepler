d78673874d149308200a66adf32b89926604b1b9
kenny oh <kennyoh517@gmail.com>
2020-02-22 20:32:52 -0500

    add api prefix route to data dir

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(path.join(__dirname, 'data')));
+app.use('/api', express.static(path.join(__dirname, 'data')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
